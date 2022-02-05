0d05ce2d134da6debfb0d6a82b06187e2a4d1781
lifeofsoumya <mondalsoumya2001@gmail.com>
2022-02-05 12:51:25 +0530

    temporary folder name changed

--- cronjob.sh
+++ cronjob.sh
@@ -10,13 +10,13 @@ crontab -e  # opening cron editor
 cronfunc(){
 
     cd '/cronassets/' # entering the folder where all files exist
-    mkdir imgfolder # creating a directory to temporariliy store sorted files
+    mkdir tempfolder # creating a directory to temporariliy store sorted files
     for x in *.JPG *.jpg *.TIF *.tif *.PNG *.png *.BMP *.bmp *.GIF *.gif *.IMG *.img *.JPEG *.jpeg ; # run a loop for files having extension of these format
     do
-        mv x imgfolder # adding images to a folder after sorting
+        mv x tempfolder # adding images to a folder after sorting
     done
     timestamp = date +%b-%d-%y # Timestamp var to show when the backup was done
-    tar -cvf ./backups/backedup-$timestamp.tar.gz /imgfolder #backing up the directory where sorted files are kept
-    rmdir imgfolder # deleting the file sorted directory after backup is done
+    tar -cvf ./backups/backedup-$timestamp.tar.gz /tempfolder #backing up the directory where sorted files are kept
+    rmdir tempfolder # deleting the file sorted directory after backup is done
 
 }
