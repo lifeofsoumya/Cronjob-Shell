b88672f98851a049d7fac3f0f70f0cf15cd87178
lifeofsoumya <mondalsoumya2001@gmail.com>
2022-02-05 11:32:32 +0530

    make directory, backup and then delete

--- cronjob.sh
+++ cronjob.sh
@@ -10,8 +10,10 @@ crontab -e  # opening cron editor
 cronfunc(){
 
     cd '/cronassets/' # entering the folder where all files exist
+    mkdir imgfolder # creating a directory to temporariliy store sorted files
     for x in *.JPG *.jpg *.TIF *.tif *.PNG *.png *.BMP *.bmp *.GIF *.gif *.IMG *.img *.JPEG *.jpeg ; do # run a loop for files having extension of these format
-    
 
+    tar -cvf ./backups/backedup.tar.gz /imgfolder #backing up the directory where sorted files are kept
+    rmdir imgfolder # deleting the file sorted directory after backup is done
 
 }
